567f2fbddc78a7a257672cfcf9f8827665588224
denyskononenko <kononenkody@gmail.com>
2021-08-14 18:05:37 +0200

    feat: creation of cloud points of Zernike 3D functions is added.

--- descriptors/zernike3D.py
+++ descriptors/zernike3D.py
@@ -49,7 +49,7 @@ class Zernike3D:
         Z_on_grid = np.array([[[np.real(Z(x, y, z)) for x in s] for y in s] for z in s])
         return Z_on_grid
 
-    def zfunction_point_cloud(self,  n: int, l: int, m: int) -> np.array:
+    def zfunction_point_cloud(self, n: int, l: int, m: int) -> np.array:
         """Calculate 3D Zernike function in the form of point cloud."""
         # make point cloud vertices
         s = np.arange(-1, 1 + self.ds, self.ds)
@@ -57,10 +57,19 @@ class Zernike3D:
         Z_point_cloud = np.array([[x, y, z, np.real(Z(x, y, z))] for x in s for y in s for z in s if x**2 + y**2 + z**2 <= 1])
         # coloring setup
         norm = mpl.colors.Normalize(vmin=np.min(Z_point_cloud[:,3]), vmax=np.max(Z_point_cloud[:,3]))
-        cmap = cm.viridis
+        cmap = cm.winter
         m = cm.ScalarMappable(norm=norm, cmap=cmap)
         # add color to the point cloud 
         # return format: x y z r ,g, b, Z_value
         Z_point_cloud_colored = np.array([[x, y, z, m.to_rgba(Z_v)[0], m.to_rgba(Z_v)[1], m.to_rgba(Z_v)[2], Z_v] for x, y, z, Z_v in Z_point_cloud])
         
         return Z_point_cloud_colored
+
+    def allowed_modes(self, nmax: int) -> list:
+        """Generate list of (n, l, m) allowed modes for given maximum order of n: nmax."""
+        return [(n, l, m) for n in range(nmax + 1) for l in range(nmax + 1) for m in range(-l, l+1) if (n - l) % 2 == 0 and n >= l]
+
+    def save_zfunction_point_cloud(self, n: int, l: int, m: int, pathtosave: str):
+        """Save point cloud for given Zernike 3D function."""
+        Z_point_cloud_colored = self.zfunction_point_cloud(n, l, m)
+        np.save(f"{pathtosave}/{n}_{l}_{m}.npy", Z_point_cloud_colored)
